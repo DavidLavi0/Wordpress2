3c24fe2fc70d931a5f9f5486bdf6f59f3ee2f665
David Lavička <David.Lavicka@student.souepl.cz>
2025-10-09 11:29:31 +0200

    uprava 5

--- wp-content/themes/mojesablona/page.php
+++ wp-content/themes/mojesablona/page.php
@@ -1,25 +1,17 @@
-<?php $theme_url = get_template_directory_uri(); ?>
 <!DOCTYPE html>
-
+<?php $theme_url = get_template_directory_uri(); ?>
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php  
-            if ( is_front_page() ) { // Pokud je to domovská stránka (statická stránka)
-                echo get_the_title( get_option( 'page_on_front' ) ); // Získá název statické domovské stránky
-            } else {
-                wp_title( '|', true, 'right' ); // Standardní titulek pro ostatní stránky
-            }
-         ?></title>
-         <?php wp_head(); ?>
-         <script src="<?= $theme_url ?>/script/script.js" defer></script>
-         <link rel="stylesheet" href="<?= $theme_url ?>/style.css">
-         <link rel="stylesheet" href="<?= $theme_url ?>/footer.css">
 
+         <script src="<?= $theme_url?>/script/script.js" defer></script>
+         <link rel="stylesheet" href="<?= $theme_url ?>/style.css">
+         <link rel="stylesheet" href="<?= $theme_url ?>/style/footer.css">
+         <?php wp_head(); ?>
     </head>
     <body>
-        <?=
+        <?php
             get_header();
         ?>
 
